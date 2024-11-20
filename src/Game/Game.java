eaaa69e81611ab4ad62377e4bc496fe223654edf
ilian.desbois <ilian.desbois@etu.u-paris.fr>
2024-11-20 11:19:54 +0100

    Push

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,32 +1,45 @@
 package Game;
 
 import Utils.Color;
+import Utils.IO;
 
 public class Game {
 
     private int taille;
     private Board board;
+    private IO io;
+    private boolean partieFinie = false;
 
     public Game(int taille) {
         this.taille = taille;
         this.board = new Board(taille);
+        this.io = new IO();
     }
 
     public void startSession() {
-        System.out.println(board.toString());
-        board.addBoule(new Boule(new Coordonnees(1,5), Color.Black));
-        System.out.println(board.toString());
+        jouePartie();
     }
 
     public void jouePartie() { //Permet de jouer une partie
         while (true) {
-            if(partieFini())
+            System.out.println(board.toString());
+            executeCommande(io.getCommande());
+            if(checkPartieFinie())
                 break;
         }
     }
 
-    private boolean partieFini() { //vérifie que la partie est fini
-        return false;
+    private void executeCommande(String commande) {
+        if(commande.startsWith("quit")) {
+            partieFinie = true;
+        } else if(commande.startsWith("boardsize")) {
+            taille = Integer.parseInt(commande.split(" ")[1]);
+            board = new Board(taille);
+        }
+    }
+
+    private boolean checkPartieFinie() { //vérifie que la partie est fini
+        return partieFinie;
     }
 
 }
---
+++ src/Utils/IO.java
@@ -0,0 +1,45 @@
+package Utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class IO {
+
+    private Scanner scanner;
+    private ArrayList<String> listeCommande;
+
+    public IO() {
+        scanner = new Scanner(System.in);
+        initialiseCommande();
+    }
+
+    public void initialiseCommande() {
+        listeCommande = new ArrayList<>();
+        listeCommande.add("quit");
+        listeCommande.add("boardsize");
+    }
+
+    public String getCommande() {
+        System.out.println("Veuillez saisir le commande : ");
+        String command = " ";
+        Boolean commandeAvailable = false;
+        while(true) {
+            command = scanner.nextLine();
+            for (String s : listeCommande) {
+                if(command.startsWith(s))
+                    commandeAvailable = true;
+            }
+            if(commandeAvailable == true)
+                break;
+            System.out.print("Veuillez saisir une commande existante : \n(Commande existante : ");
+            for(String s : listeCommande) {
+                System.out.print(s + ", ");
+            }
+            System.out.println(")");
+        }
+        return command;
+    }
+
+
+}
