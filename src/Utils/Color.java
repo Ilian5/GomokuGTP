4f9186aec2c1a6156277ea6a44c8ef08b215792d
Ilian5 <ilian.desbois@etu.u-paris.fr>
2024-11-24 23:23:12 +0100

    Modification de Game.java et IO.java

--- src/Utils/Color.java
+++ src/Utils/Color.java
@@ -1,18 +1,37 @@
 package Utils;
+
 public enum Color {
 
     Black('B'),
     White('W');
 
     private char color;
+
+    /**
+     * Constructeur de l'énumération {@code Color}.
+     * Initialise l'énumération avec le caractère représentant la couleur.
+     *
+     * @param color Le caractère représentant la couleur ('B' pour noir, 'W' pour blanc).
+     */
     Color(char color) {
         this.color = color;
     }
 
+    /**
+     * Retourne le caractère associé à la couleur.
+     *
+     * @return Le caractère représentant la couleur ('B' ou 'W').
+     */
     public char getColorChar() {
         return color;
     }
 
+    /**
+     * Retourne le caractère associé à la couleur.
+     * Cette méthode est identique à {@code getColorChar()}.
+     *
+     * @return Le caractère représentant la couleur ('B' ou 'W').
+     */
     public char toChar() {
         return color;
     }
