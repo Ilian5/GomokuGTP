603afd44440778198cd1525e42a7e057ff18e95a
Ilian5 <ilian.desbois@etu.u-paris.fr>
2024-11-24 22:47:15 +0100

    Modification de Appli.java

--- src/Appli/Appli.java
+++ src/Appli/Appli.java
@@ -4,18 +4,7 @@ import java.util.Scanner;
 import Game.Game;
 
 public class Appli {
-    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Veuillez entrer une taille de tableau :");
-        int taille = 0;
-        while(true) {
-            if(sc.hasNextInt()) {
-                taille = sc.nextInt();
-                break;
-            }
-            System.out.println("Veuillez entrer uniquement un nombre");
-            sc.next();
-        }
-        new Game(taille).startSession();
+        new Game().startSession();
     }
 }
