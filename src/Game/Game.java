87b7ce0065820940c3114bfc874babdf6bf822a9
Ilian5 <ilian.desbois@etu.u-paris.fr>
2024-11-24 23:42:33 +0100

    Modification de Game.java et IO.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -3,17 +3,25 @@ package Game;
 import Utils.Color;
 import Utils.IO;
 
+/**
+ * La classe {@code Game} gère une session de jeu, permettant aux joueurs de jouer au jeu Gomoku.
+ * Elle permet de démarrer une partie, exécuter des commandes, et de gérer l'état du jeu.
+ */
 public class Game {
 
     private static final int TAILLE_DEFAULT = 19; //Taille du plateau par défaut
-    private static final int NB_BOULLE_ALIGNE = 5; //Nombre de boulle à aligner pour gagner
+    private static final int NB_BOULE_ALIGNE = 5; //Nombre de boules à aligner pour gagner
 
     private Board board;
-    private IO io;
+    private final IO io;
     private int taille;
     private boolean gameFinished;
     private boolean gameStarted;
 
+    /**
+     * Constructeur de la classe {@code Game}. Initialise le plateau de jeu, la taille par défaut,
+     * et crée une instance de {@code IO} pour la gestion des entrées utilisateur.
+     */
     public Game() {
         this.taille = TAILLE_DEFAULT;
         this.board = new Board(taille);
@@ -22,22 +30,31 @@ public class Game {
         this.gameStarted = false;
     }
 
+    /**
+     * Démarre une nouvelle session de jeu. La taille du plateau est déterminée par l'utilisateur.
+     */
     public void startSession() {
         taille = io.getTailleDebutPartie();
         jouePartie();
     }
 
+    /**
+     * Permet de jouer une partie en boucle, en exécutant les commandes de l'utilisateur jusqu'à ce que la partie soit terminée.
+     */
     public void jouePartie() { //Permet de jouer une partie
         do {
             executeCommande(io.getCommande());
         } while (!checkPartieFinie());
     }
 
+    /**
+     * Exécute la commande entrée par l'utilisateur en fonction de l'action spécifiée.
+     * @param commande La commande entrée par l'utilisateur.
+     */
     private void executeCommande(String commande) {
         String[] parts = commande.split(" ", 2);
         String action = parts[0];
         String argument = parts.length > 1 ? parts[1] : null;
-
         switch (action) {
             case "quit":
                 gameFinished = true;
@@ -73,6 +90,10 @@ public class Game {
         }
     }
 
+    /**
+     * Définit la taille du plateau de jeu si aucune partie n'a commencé.
+     * @param argument Taille du plateau sous forme de chaîne de caractères.
+     */
     private void commandBoardSize(String argument) {
         if (gameStarted) {
             System.out.println("Une partie est déjà en cours ! Veuillez la terminer ou y mettre fin (partiestop) !");
@@ -91,46 +112,81 @@ public class Game {
         }
     }
 
+    /**
+     * Joue un tour de jeu en vérifiant la validité du mouvement avant de l'appliquer.
+     * @param commande La commande contenant la couleur et la position du mouvement.
+     */
     private void playTour(String commande) {
-        if(commande.split(" ").length == 3) {
-            commande = commande.toUpperCase();
-            char color = commande.split(" ")[1].charAt(0);
-            String mouvement = commande.split(" ")[2];
-            if(!(color == 'B' || color == 'W')) {
-                System.out.println("Couleur incorrect, couleur possible : B (Black) | W (White)");
-                return;
-            }
-            if(!(mouvement.charAt(0) - 'A' <= (taille - 1) && Integer.parseInt(mouvement.substring(1)) <= taille - 1)) {
-                System.out.println("Mouvement incorrect, Mouvement disponible compris entre A0 et " + ((char) ('A' + taille - 1)) + (taille - 1) + "\n" + getMouvementDisponible());
-                return;
-            }
-            if(!mouvementPossible(mouvement)) {
-                System.out.println("Mouvement impossible, une boule est déjà à ces coordonnées !");
-                return;
-            }
-
-            board.addBoule(new Boule(new Coordonnees((int) mouvement.charAt(0) - 'A', Integer.parseInt(mouvement.substring(1))), (color == 'B' ? Color.Black : Color.White)));
-        } else {
+        String[] parts = commande.toUpperCase().split(" ");
+        if(parts.length != 3) {
             System.out.println("Commande incorrecte, veuillez entrer une couleur et un mouvement !");
+            return;
         }
+
+        char color = parts[1].charAt(0);
+        String mouvement = parts[2];
+
+        if(color != 'B' && color != 'W') {
+            System.out.println("Couleur incorrect, couleur possible : B (Black) | W (White)");
+            return;
+        }
+        if(!isMouvementPossible(mouvement)) {
+            System.out.println("Mouvement incorrect, Mouvement disponible compris entre A0 et " +
+                    ((char) ('A' + taille - 1)) + (taille - 1) + "\n" + getMouvementDisponible());
+            return;
+        }
+        if(!isPositionNonOccupee(mouvement)) {
+            System.out.println("Mouvement impossible, une boule est déjà à ces coordonnées !");
+            return;
+        }
+        Coordonnees coord = new Coordonnees((int) mouvement.charAt(0) - 'A', Integer.parseInt(mouvement.substring(1)));
+        Color bouleColor = (color == 'B' ? Color.Black : Color.White);
+        board.addBoule(new Boule(coord, bouleColor));
     }
 
-    private boolean mouvementPossible(String mouvement) {
+    /**
+     * Vérifie si le mouvement est possible sur le plateau, c'est-à-dire si les coordonnées sont valides.
+     * @param mouvement Le mouvement sous forme de chaîne (ex: "A1").
+     * @return {@code true} si le mouvement est valide, {@code false} sinon.
+     */
+    private boolean isMouvementPossible(String mouvement) {
+        return mouvement.charAt(0) - 'A' <= (taille - 1) &&
+                Integer.parseInt(mouvement.substring(1)) <= taille - 1;
+    }
+
+    /**
+     * Vérifie si la position donnée par le mouvement est déjà occupée par une boule.
+     * @param mouvement Le mouvement sous forme de chaîne (ex: "A1").
+     * @return {@code true} si la position est libre, {@code false} si elle est occupée.
+     */
+    private boolean isPositionNonOccupee(String mouvement) {
         int mouvementLettre = mouvement.charAt(0) - 'A';
-        int mouvementChiffre = Integer.parseInt(mouvement.substring(1));
-        boolean mouvementPossible = true;
-        for(Boule b : board.getBoule()) {
-            if(b.getCoordonnees().getX() == mouvementLettre && b.getCoordonnees().getY() == mouvementChiffre) {
-                mouvementPossible = false;
-            }
+        int mouvementChiffre;
+
+        try {
+            mouvementChiffre = Integer.parseInt(mouvement.substring(1));
+        } catch (NumberFormatException e) {
+            // Si le mouvement est invalide, retourne false immédiatement
+            return false;
         }
-        return mouvementPossible;
+
+        return board.getBoule().stream()
+                .noneMatch(b -> b.getCoordonnees().getX() == mouvementLettre &&
+                        b.getCoordonnees().getY() == mouvementChiffre);
     }
 
+    /**
+     * Vérifie si la partie est terminée.
+     * @return {@code true} si la partie est terminée, {@code false} sinon.
+     */
     private boolean checkPartieFinie() { //vérifie que la partie est fini
-        return partieFinie;
+        return gameFinished;
     }
 
+    /**
+     * Génère et retourne une chaîne représentant les mouvements disponibles sur le plateau.
+     * @return Une chaîne contenant les mouvements disponibles.
+     */
     private String getMouvementDisponible() {
         StringBuilder s = new StringBuilder("[\n");
         for(int i = 0; i < taille; ++i){
